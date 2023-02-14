d171e6ff1b3edb4a52f1666e6a25799cbb369de8
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-02-14 17:56:53 +0100

    Using new React routing concept

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
-import './App.css';
+import { RootLayout, HelpLayout } from './Layouts/index';
 import { Home, About } from './pages/index';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <RootLayout /> }>
+      <Route index element={ <Home /> } />
+      <Route path='about' element={ <About /> } />
+      <Route path='help' element={ <HelpLayout /> }>
+        <Route path='faq' />
+        <Route path='contact' />
+      </Route>
+    </Route>
+  )
+)
 
 
 
 const App = () => {
   return (
-    <main className='app'>
-      <header>
-        <nav>
-          <h1>React-RD</h1>
-          <NavLink 
-            to='/'
-            className='nav-link' 
-          >Home</NavLink>
-          <NavLink 
-            to='about' 
-            className='nav-link'
-          >About</NavLink>
-        </nav>
-      </header>
-      <Routes>
-        <Route index element={ <Home /> } />
-        <Route path='about' element={ <About /> } />
-      </Routes>
-    </main>
+    <RouterProvider router={ router } />
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -10,8 +9,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );

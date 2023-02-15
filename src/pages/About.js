91b2ed4d461e3f150072b10343463d6b911c9a96
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-02-15 15:20:37 +0100

    Add navigate to the About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 
 
 const About = () => {
+  const [user, setUser] = useState('Choaib');
+
+  if (!user) {
+    return <Navigate to='/' replace={ true } />
+  }
+
   return (
     <div className='app__about'>
         <h2>About Us</h2>
@@ -24,6 +31,19 @@ const About = () => {
            Cillum nulla nostrud commodo amet in. Ad eiusmod laboris 
            exercitation nisi ea et qui deserunt.
         </p>
+        <button 
+          style={{
+            padding: '4px 8px',
+            borderRadius: '6px',
+            background: 'orangered',
+            color: '#e4e4e4',
+            fontSize: '16px',
+            border: '1px solid orangered',
+            outline: 'none',
+            cursor: 'pointer'
+          }}
+          onClick={ () => setUser(null) }
+        >Logout</button>
     </div>
   );
 };
